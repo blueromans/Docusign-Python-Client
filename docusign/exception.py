7fbe49ec415b0981f07263a3d5855e256a0ad9d5
Yaşar Özyurt <yasar.ozyurt@bilgeadam.com>
2024-07-06 02:48:11 +0300

    feat:Version fix.

--- docusign/exception.py
+++ docusign/exception.py
@@ -2,6 +2,7 @@ class ErrorCodes:
     CODE_FROM_URL_ERROR = 'Code from url error!'
     INVALID_ATTRIBUTE = 'Invalid attribute!'
     ENVIRONMENT_ERROR = 'Invalid environment!'
+    TOKEN_ERROR = 'Invalid token!'
     ENCODED_KEYS_ERROR = 'Invalid encoded keys!'
     API_KEY_ERROR = 'Invalid api key!'
     API_URL_ERROR = 'Invalid api url!'
